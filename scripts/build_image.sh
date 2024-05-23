2de91f39483966b268dbd98bd094f914d0484a10
Stephen A. Davis <stephen@webdavis.io>
2024-05-23 00:49:52 -0600

    Define required usage and provide `-h|--help` flag

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -6,6 +6,47 @@ set -eo pipefail
 DOCKER_CMD='sudo docker'
 REPO_ADDRESS="webdavis/docker-s6-overlay"
 S6_OVERLAY_VERSION_FILE='s6_overlay_version.json'
+SCRIPT_NAME="${BASH_SOURCE##*/}"
+
+help() {
+    printf "%s\\n" "
+${SCRIPT_NAME}: Package multi-platform Docker images with s6-overlay
+
+All of the following flags must be specified:
+
+    -p|--platform <platform>                                The buildx platform being used (passed to --platform)
+    -i|--image <image>                                      The name of the official base image
+    -v|--image-version <image_version>                      The version of the official base image
+    -a|--s6-overlay-architecture <s6_overlay_architecture>  The architecture of the s6 overlay tarball. See s6_architecture_mappings.json
+
+Example using short flags:
+    ${SCRIPT_NAME} -p linux/amd64 -i alpine -v 3.19 -a 3.1.6.2
+
+Example using long flags:
+    ${SCRIPT_NAME} --platform linux/amd64 --image alpine --image-version 3.19 --s6-overlay-architecture 3.1.6.2"
+}
+
+verify_script_arguments() {
+  if (( $# != 8 )); then
+    printf "%s\\n" "
+${SCRIPT_NAME}: Invalid usage
+
+You must specify the following flags:
+
+    -p <platform>                (or --platform <platform>)
+    -i <image>                   (or --image <image>)
+    -v <image_version>           (or --image-version <image_version>)
+    -a <s6_overlay_architecture> (or --s6-overlay-architecture <s6_overlay_architecture>)
+
+Example using short flags:
+    ${SCRIPT_NAME} -p linux/amd64 -i alpine -v 3.19 -a 3.1.6.2
+
+Example using long flags:
+    ${SCRIPT_NAME} --platform linux/amd64 --image alpine --image-version 3.19 --s6-overlay-architecture 3.1.6.2
+" >&2
+  exit 1
+  fi
+}
 
 get_repo_root_directory() {
   git rev-parse --show-toplevel
@@ -16,8 +57,8 @@ load_s6_overlay_version() {
 }
 
 parse_command_line_arguments() {
-  local short='p:i:v:a:'
-  local long='platform:,image:,image-version:,s6-overlay-architecture:'
+  local short='p:i:v:a:h'
+  local long='platform:,image:,image-version:,s6-overlay-architecture:,help'
 
   OPTIONS="$(getopt -o "$short" --long "$long" -- "$@")"
   eval set -- "$OPTIONS"
@@ -40,6 +81,10 @@ parse_command_line_arguments() {
         S6_OVERLAY_ARCHITECTURE="$2"
         shift 2
         ;;
+      -h | --help)
+        help
+        exit 0
+        ;;
       --)
         shift
         break
@@ -50,6 +95,8 @@ parse_command_line_arguments() {
         ;;
     esac
   done
+
+  verify_script_arguments "$@"
 }
 
 build_image() {
